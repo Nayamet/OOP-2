62e5fbfbc7d89375da68b3992c9e5f2eae9b5c11
DISHAN AHMED <srdishan872@gmail.com>
2020-11-25 10:06:31 +0600

    1st interface complete

---
+++ Final second lab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_second_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NormalCalculator n1 = new NormalCalculator();
+            n1.sum(10, 15);
+            n1.sub(15, 10);
+            n1.multiplication(15, 10);
+            n1.division(150, 10);
+            Console.WriteLine();
+            ScientificCalculator s1 = new ScientificCalculator();
+            s1.XtoY(3, 4);
+            s1.SquareRoot(9);
+            
+        }
+    }
+}
---
+++ Final second lab/ScientificCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_second_lab
+{
+    class ScientificCalculator : IScientificCalculatorInterface
+    {
+        public ScientificCalculator()
+        {
+            Console.WriteLine("Scientific calculator.");
+        }
+        
+        public double SquareRoot(double x)
+        {
+            Console.WriteLine("Square Root : "+ Math.Sqrt(x));
+            return Math.Sqrt(x);
+        }
+
+        public int XtoY(int x, int y)
+        {
+            Console.WriteLine("X To Y : " + Convert.ToInt32(Math.Pow(x, y)));
+            return Convert.ToInt32(Math.Pow(x, y));
+        }
+
+       
+
+        
+    }
+}
